7dd1cfda616c382eb24ef787d927e1f05265e03f
Diego B <dbsmendes@gmail.com>
2023-02-28 21:08:52 -0300

    TASKS SOLVED

--- TASK 1.js
+++ TASK 1.js
@@ -1,4 +1,7 @@
-let i = 13; soma = 0; k = 0
+// DADO O CÓDIGO ABAIXO, QUAL VALOR SERÁ IMPRESSO NO CONSOLE.LOG(SOMA)?
+// RESPOSTA: 91
+
+let i = 13; soma = 0; k = 0;
 
 while (k < i) {
     k = k + 1;
